442da6312e6f4aaef0da38ff1cacaa05759efe49
YOUR_NAME <YOUR_EMAIL>
2023-07-26 14:02:34 +0300

    create TaskForm and TagForm

---
+++ tasks/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+
+from .models import Task, Tag
+
+
+class TaskForm(forms.ModelForm):
+    tags = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+    deadline = forms.DateTimeField(
+        widget=forms.DateTimeInput(
+            attrs={'type': 'datetime-local'}
+        ),
+        required=False
+    )
+
+    class Meta:
+        model = Task
+        fields = "__all__"
+
+
+class TagForm(forms.ModelForm):
+    class Meta:
+        model = Tag
+        fields = "__all__"
