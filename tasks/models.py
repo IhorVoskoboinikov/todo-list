277f0af3c8744695c9a6dac249d48e6584210b77
YOUR_NAME <YOUR_EMAIL>
2023-07-26 14:49:03 +0300

    added is_done = models.BooleanField(default=False) in Task model

--- tasks/models.py
+++ tasks/models.py
@@ -5,7 +5,7 @@ class Task(models.Model):
     content = models.CharField(max_length=255)
     create_at = models.DateTimeField(auto_now_add=True)
     deadline = models.DateTimeField(blank=True, null=True)
-    is_done = models.BooleanField()
+    is_done = models.BooleanField(default=False)
     tags = models.ManyToManyField("Tag", related_name="tasks")
 
     class Meta:
